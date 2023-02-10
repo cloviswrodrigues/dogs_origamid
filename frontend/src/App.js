fc2d621942b1cbf85912fc85f9d8d5b97032ef62
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-02-09 23:41:14 -0300

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PhotoGrid from "./components/Photos/PhotoGrid";
+import LoginScreen from "./components/Login/LoginScreen";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<PhotoGrid />} />
+          <Route path="/login" element={<LoginScreen />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
