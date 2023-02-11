40bc2e7171b4c68e080c925033a251ab42d1651f
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-02-11 18:10:15 -0300

    stylized login screen

--- frontend/src/components/Login/LoginScreen.js
+++ frontend/src/components/Login/LoginScreen.js
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+
+import style from "./LoginScreen.module.css";
+
 const LoginScreen = () => {
-  return <div>tela de login</div>;
+  return (
+    <main className={style.login}>
+      <div className={style.loginImg}></div>
+      <div className={style.loginForm}>
+        <h1 className="title-1">Login</h1>
+        <form action="">
+          <label htmlFor="user">Usuário</label>
+          <input type="text" name="user" />
+          <label htmlFor="password">Senha</label>
+          <input type="password" name="password" />
+          <button type="submit" className="btn btn-medium">
+            Entrar
+          </button>
+        </form>
+        <Link to="/lost-password" className={style.lostPassword}>
+          Perdeu a Senha?
+        </Link>
+        <div className={style.register}>
+          <h2 className="title-2">Cadastre-se</h2>
+          <p>Ainda não possui conta? Cadastre-se no site.</p>
+          <Link to="/register" className="btn btn-small">
+            Cadastro
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default LoginScreen;
