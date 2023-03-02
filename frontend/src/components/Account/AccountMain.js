ff7239c9911966c88e88e55b6b77b9279280b803
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-03-01 23:59:39 -0300

    implemented request get account photos

--- frontend/src/components/Account/AccountMain.js
+++ frontend/src/components/Account/AccountMain.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { PHOTOS_GET } from "../../Api";
+import PhotoGrid from "../Photos/PhotoGrid";
 
 const AccountMain = ({ setPageTitle }) => {
+  const [photos, setPhotos] = React.useState(null);
+
+  async function getAccountPhotos() {
+    const page = 1;
+    const total = 6;
+    const user = 15;
+
+    try {
+      setPhotos(null);
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      console.log("url2 ", url);
+      console.log("options ", options);
+      const response = await fetch(url, options);
+      const json = await response.json();
+      if (json.length) {
+        setPhotos(json);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
   React.useEffect(() => {
     setPageTitle("Minha Conta");
+    getAccountPhotos();
   }, []);
 
-  return <div>conta</div>;
+  return (
+    <section>
+      {photos ? (
+        <PhotoGrid photos={photos} />
+      ) : (
+        <p>Não há nenhumm foto para ser exibida</p>
+      )}
+    </section>
+  );
 };
 
 export default AccountMain;
--- frontend/src/components/Photos/PhotoGrid.js
+++ frontend/src/components/Photos/PhotoGrid.js
@@ -1,5 +1,8 @@
-const PhotoGrid = () => {
-  return <main className="container">teste</main>;
+import React from "react";
+
+const PhotoGrid = ({ photos }) => {
+  console.log("phts: ", photos);
+  return <div className="container">{photos[0].src}</div>;
 };
 
 export default PhotoGrid;
