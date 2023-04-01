70b08b266cb1c9d8054338961d2d27e4f86a4e2a
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-03-31 23:18:08 -0300

    implemented logif lost password

--- frontend/src/components/Login/LostPassword.js
+++ frontend/src/components/Login/LostPassword.js
@@ -1,28 +1,71 @@
 import React from "react";
 import style from "./LoginScreen.module.css";
+import { PASSWORD_LOST } from "../../Api";
+
+const SUCCESS = "success";
+const ERROR = "error";
 
 const LostPassword = () => {
   const [login, setLogin] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [responseStatus, setResponseStatus] = React.useState(null);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    setResponseStatus(null);
+    setLoading(true);
+    const host = window.location.origin;
+    const urlCallBack = `${host}/login/resetar`;
+    const body = {
+      login,
+      url: urlCallBack,
+    };
+    const { url, options } = PASSWORD_LOST(body);
+    const response = await fetch(url, options);
+    if (response.ok) {
+      setResponseStatus(SUCCESS);
+    } else {
+      setResponseStatus(ERROR);
+    }
+    setLoading(false);
   }
 
   return (
     <div className={`animeLeft ${style.loginForm}`}>
       <h1 className="title-1">Perdeu a senha</h1>
-      <form action="">
-        <label htmlFor="login">Email / Usuário</label>
-        <input
-          type="text"
-          name="login"
-          value={login}
-          onChange={({ target }) => setLogin(target.value)}
-        />
-        <button type="submit" className="btn btn-medium" onClick={handleSubmit}>
-          Enviar Email
-        </button>
-      </form>
+      {responseStatus === SUCCESS ? (
+        <p className={style.successForm}>Email enviado</p>
+      ) : (
+        <>
+          <form action="">
+            <label htmlFor="login">Email / Usuário</label>
+            <input
+              type="text"
+              name="login"
+              value={login}
+              onChange={({ target }) => setLogin(target.value)}
+            />
+            {loading ? (
+              <button type="submit" className="btn btn-medium" disabled>
+                Enviando...
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="btn btn-medium"
+                onClick={handleSubmit}
+              >
+                Enviar Email
+              </button>
+            )}
+          </form>
+          {responseStatus === ERROR && (
+            <span className={style.erroForm}>
+              Ocorreu erro no envio de email
+            </span>
+          )}
+        </>
+      )}
     </div>
   );
 };
