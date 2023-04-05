2879f90024a4895fa08a4413fc2098c92cacec5b
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-04 23:27:01 -0300

    implemented PhotoProfile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { UserStorage } from "./Context/UserContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PhotoHome from "./components/Photos/PhotoHome";
+import PhotoProfile from "./components/Photos/PhotoProfile";
 import Login from "./components/Login/Login";
 import Account from "./components/Account/Account";
 
@@ -15,6 +16,7 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<PhotoHome />} />
+            <Route path="/perfil/:user" element={<PhotoProfile />} />
             <Route path="/login/*" element={<Login />} />
             <Route path="/conta/*" element={<Account />} />
           </Routes>
