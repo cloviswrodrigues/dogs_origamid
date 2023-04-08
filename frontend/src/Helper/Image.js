96f6c28e7b5c0589095c85d85802416bcc2a04df
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-07 23:42:49 -0300

    fix animation skeleton image

--- frontend/src/Helper/Image.js
+++ frontend/src/Helper/Image.js
@@ -6,14 +6,14 @@ const Image = ({ ...props }) => {
   const [skeleton, setSkeleton] = React.useState(true);
 
   function handleLoad({ target }) {
-    //setSkeleton(false);
-    //target.style.opacity = 1;
+    setSkeleton(false);
+    target.style.opacity = 1;
   }
 
   return (
     <div className={styles.wrapper}>
       {skeleton && <div className={styles.skeleton}></div>}
-      {/* <img onLoad={handleLoad} className={styles.img} {...props} /> */}
+      <img onLoad={handleLoad} className={styles.img} {...props} />
     </div>
   );
 };
