b02fe9017365c73f0a4d3843fc604ee6214478c8
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-09 23:13:47 -0300

    implemented partial pagination in photo grid

--- frontend/src/components/Photos/PhotoHome.js
+++ frontend/src/components/Photos/PhotoHome.js
@@ -6,21 +6,26 @@ import { PHOTOS_GET } from "../../Api";
 
 import styles from "./PhotoHome.module.css";
 
-const Home = () => {
-  const [photos, setPhotos] = React.useState(null);
-  const [loading, setLoading] = React.useState(true);
+const TOTAL_PHOTO = 6;
 
-  async function getAccountPhotos() {
-    const page = 1;
-    const total = 6;
+const Home = () => {
+  const [photos, setPhotos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfite] = React.useState(true);
+
+  async function getAccountPhotos(pageNumber) {
+    const page = pageNumber;
+    const total = TOTAL_PHOTO;
 
     try {
-      setPhotos(null);
       const { url, options } = PHOTOS_GET({ page, total });
       const response = await fetch(url, options);
       const json = await response.json();
-      if (json.length) {
-        setPhotos(json);
+      const numberPhoto = json.length;
+      if (numberPhoto) {
+        setPhotos((photos) => [...photos, ...json]);
+        if (numberPhoto < TOTAL_PHOTO) setInfite(false);
       }
     } catch (error) {
     } finally {
@@ -28,8 +33,39 @@ const Home = () => {
     }
   }
 
+  function infiteScroll(e) {
+    let wait = false;
+    const scroll = window.scrollY;
+    const height = document.body.offsetHeight - window.innerHeight;
+
+    if (infinite) {
+      if (scroll > height * 0.75 && !wait) {
+        console.log("busca novas fotos");
+        setPages((pages) => [...pages, pages.length + 1]);
+
+        wait = true;
+        setTimeout(() => {
+          wait = false;
+        }, 500);
+      }
+    }
+  }
+
   React.useEffect(() => {
-    getAccountPhotos();
+    console.log("2 effect");
+    const lastPage = pages.slice(-1);
+    //getAccountPhotos(lastPage);
+  }, [pages]);
+
+  React.useEffect(() => {
+    window.addEventListener("wheel", infiteScroll);
+    window.addEventListener("scroll", infiteScroll);
+    getAccountPhotos(1);
+
+    return () => {
+      window.removeEventListener("wheel", infiteScroll);
+      window.removeEventListener("scroll", infiteScroll);
+    };
   }, []);
 
   return (
