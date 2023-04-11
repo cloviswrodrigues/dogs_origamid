0e07950caeb2e9997bff8d0b5a0a779e4f7fca94
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-10 23:59:40 -0300

    fixing photo grid

--- frontend/src/components/Photos/PhotoGrid.js
+++ frontend/src/components/Photos/PhotoGrid.js
@@ -1,16 +1,52 @@
 import React from "react";
+
 import styles from "./PhotoGrid.module.css";
+import { PHOTOS_GET } from "../../Api";
 import PhotoModal from "./PhotoModal";
-
 import Image from "../../Helper/Image";
+import Loader from "../Loader";
 
-const PhotoGrid = ({ photos }) => {
+const TOTAL_PHOTO = 6;
+
+const PhotoGrid = ({ page, setInfite }) => {
+  const [photos, setPhotos] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
   const [showModal, setShowModal] = React.useState(false);
   const [idPhoto, setIdPhoto] = React.useState(null);
   function openPhotoModal(idPhoto) {
     setIdPhoto(idPhoto);
     setShowModal(true);
   }
+
+  async function getAccountPhotos() {
+    const total = TOTAL_PHOTO;
+
+    try {
+      const { url, options } = PHOTOS_GET({ page, total });
+      const response = await fetch(url, options);
+      const json = await response.json();
+      const numberPhoto = json.length;
+      if (numberPhoto) {
+        setPhotos([...json]);
+        console.log("page ==========>", page);
+        console.log("numberPhoto ==========>", numberPhoto);
+        console.log("TOTAL_PHOTO ==========>", TOTAL_PHOTO);
+        if (numberPhoto < TOTAL_PHOTO) {
+          console.log("entrei no if");
+          setInfite(false);
+        }
+      }
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  React.useEffect(() => {
+    getAccountPhotos();
+  }, []);
+
+  if (loading) return <Loader />;
   return (
     <>
       <section className={`animeLeft ${styles.photoGrid}`}>
--- frontend/src/components/Photos/PhotoHome.js
+++ frontend/src/components/Photos/PhotoHome.js
@@ -1,44 +1,20 @@
 import React from "react";
 
-import Loader from "../../components/Loader";
 import PhotoGrid from "./PhotoGrid";
-import { PHOTOS_GET } from "../../Api";
 
 import styles from "./PhotoHome.module.css";
 
-const TOTAL_PHOTO = 6;
-
 const Home = () => {
-  const [photos, setPhotos] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
   const [pages, setPages] = React.useState([1]);
   const [infinite, setInfite] = React.useState(true);
-
-  async function getAccountPhotos(pageNumber) {
-    const page = pageNumber;
-    const total = TOTAL_PHOTO;
-
-    try {
-      const { url, options } = PHOTOS_GET({ page, total });
-      const response = await fetch(url, options);
-      const json = await response.json();
-      const numberPhoto = json.length;
-      if (numberPhoto) {
-        setPhotos((photos) => [...photos, ...json]);
-        if (numberPhoto < TOTAL_PHOTO) setInfite(false);
-      }
-    } catch (error) {
-    } finally {
-      setLoading(false);
-    }
-  }
-
+  console.log("@pages: ", pages);
+  console.log("@infinite: ", infinite);
   function infiteScroll(e) {
     let wait = false;
-    const scroll = window.scrollY;
-    const height = document.body.offsetHeight - window.innerHeight;
-
     if (infinite) {
+      const scroll = window.scrollY;
+      const height = document.body.offsetHeight - window.innerHeight;
+
       if (scroll > height * 0.75 && !wait) {
         console.log("busca novas fotos");
         setPages((pages) => [...pages, pages.length + 1]);
@@ -52,15 +28,8 @@ const Home = () => {
   }
 
   React.useEffect(() => {
-    console.log("2 effect");
-    const lastPage = pages.slice(-1);
-    //getAccountPhotos(lastPage);
-  }, [pages]);
-
-  React.useEffect(() => {
     window.addEventListener("wheel", infiteScroll);
     window.addEventListener("scroll", infiteScroll);
-    getAccountPhotos(1);
 
     return () => {
       window.removeEventListener("wheel", infiteScroll);
@@ -70,13 +39,11 @@ const Home = () => {
 
   return (
     <main className={` ${styles.photoHome}`}>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="container">
-          <PhotoGrid photos={photos} />
-        </div>
-      )}
+      <div className="container">
+        {pages.map((page) => (
+          <PhotoGrid page={page} setInfite={setInfite} />
+        ))}
+      </div>
     </main>
   );
 };
