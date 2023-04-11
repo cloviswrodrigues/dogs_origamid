7698f72423121cd8cc623667d4dc5378ab0b9aeb
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-11 00:44:36 -0300

    fixed photo grid

--- frontend/src/components/Photos/PhotoGrid.js
+++ frontend/src/components/Photos/PhotoGrid.js
@@ -2,21 +2,15 @@ import React from "react";
 
 import styles from "./PhotoGrid.module.css";
 import { PHOTOS_GET } from "../../Api";
-import PhotoModal from "./PhotoModal";
+
 import Image from "../../Helper/Image";
 import Loader from "../Loader";
 
 const TOTAL_PHOTO = 6;
 
-const PhotoGrid = ({ page, setInfite }) => {
+const PhotoGrid = ({ page, setInfinite, openPhotoModal }) => {
   const [photos, setPhotos] = React.useState(null);
   const [loading, setLoading] = React.useState(true);
-  const [showModal, setShowModal] = React.useState(false);
-  const [idPhoto, setIdPhoto] = React.useState(null);
-  function openPhotoModal(idPhoto) {
-    setIdPhoto(idPhoto);
-    setShowModal(true);
-  }
 
   async function getAccountPhotos() {
     const total = TOTAL_PHOTO;
@@ -28,13 +22,7 @@ const PhotoGrid = ({ page, setInfite }) => {
       const numberPhoto = json.length;
       if (numberPhoto) {
         setPhotos([...json]);
-        console.log("page ==========>", page);
-        console.log("numberPhoto ==========>", numberPhoto);
-        console.log("TOTAL_PHOTO ==========>", TOTAL_PHOTO);
-        if (numberPhoto < TOTAL_PHOTO) {
-          console.log("entrei no if");
-          setInfite(false);
-        }
+        if (numberPhoto < TOTAL_PHOTO) setInfinite(false);
       }
     } catch (error) {
     } finally {
@@ -48,28 +36,24 @@ const PhotoGrid = ({ page, setInfite }) => {
 
   if (loading) return <Loader />;
   return (
-    <>
-      <section className={`animeLeft ${styles.photoGrid}`}>
-        {photos ? (
-          <div className={styles.grid}>
-            {photos.map((photo) => (
-              <div key={photo.id} className={styles.gridItem}>
-                <Image
-                  onClick={() => openPhotoModal(photo.id)}
-                  src={photo.src}
-                />
-                <span className={styles.gridItemAcess}>{photo.acessos}</span>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>Não há nenhumm foto para ser exibida...</p>
-        )}
-      </section>
-      {showModal && (
-        <PhotoModal idPhoto={idPhoto} setShowModal={setShowModal} />
+    <section className={styles.photoGrid}>
+      {photos ? (
+        <div className={styles.grid}>
+          {photos.map((photo) => (
+            <div
+              key={photo.id}
+              className={styles.gridItem}
+              onClick={() => openPhotoModal(photo.id)}
+            >
+              <Image src={photo.src} />
+              <span className={styles.gridItemAcess}>{photo.acessos}</span>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Não há nenhumm foto para ser exibida...</p>
       )}
-    </>
+    </section>
   );
 };
 
--- frontend/src/components/Photos/PhotoHome.js
+++ frontend/src/components/Photos/PhotoHome.js
@@ -1,33 +1,38 @@
 import React from "react";
 
-import PhotoGrid from "./PhotoGrid";
-
 import styles from "./PhotoHome.module.css";
+import PhotoGrid from "./PhotoGrid";
+import PhotoModal from "./PhotoModal";
 
 const Home = () => {
   const [pages, setPages] = React.useState([1]);
-  const [infinite, setInfite] = React.useState(true);
-  console.log("@pages: ", pages);
-  console.log("@infinite: ", infinite);
-  function infiteScroll(e) {
-    let wait = false;
-    if (infinite) {
-      const scroll = window.scrollY;
-      const height = document.body.offsetHeight - window.innerHeight;
+  const [infinite, setInfinite] = React.useState(true);
+  const [showModal, setShowModal] = React.useState(false);
+  const [idPhoto, setIdPhoto] = React.useState(null);
 
-      if (scroll > height * 0.75 && !wait) {
-        console.log("busca novas fotos");
-        setPages((pages) => [...pages, pages.length + 1]);
-
-        wait = true;
-        setTimeout(() => {
-          wait = false;
-        }, 500);
-      }
-    }
+  function openPhotoModal(idPhoto) {
+    console.log("open modal");
+    setIdPhoto(idPhoto);
+    setShowModal(true);
   }
 
   React.useEffect(() => {
+    let wait = false;
+    function infiteScroll() {
+      if (infinite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
+    }
     window.addEventListener("wheel", infiteScroll);
     window.addEventListener("scroll", infiteScroll);
 
@@ -35,15 +40,23 @@ const Home = () => {
       window.removeEventListener("wheel", infiteScroll);
       window.removeEventListener("scroll", infiteScroll);
     };
-  }, []);
+  }, [infinite]);
 
   return (
     <main className={` ${styles.photoHome}`}>
       <div className="container">
         {pages.map((page) => (
-          <PhotoGrid page={page} setInfite={setInfite} />
+          <PhotoGrid
+            page={page}
+            setInfinite={setInfinite}
+            openPhotoModal={openPhotoModal}
+          />
         ))}
+        {!infinite && <p>Não existem mais postagens.</p>}
       </div>
+      {showModal && (
+        <PhotoModal idPhoto={idPhoto} setShowModal={setShowModal} />
+      )}
     </main>
   );
 };
