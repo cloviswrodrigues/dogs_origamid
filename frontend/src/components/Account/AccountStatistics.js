b249db2fad9be9dec37d2f9fff4eee642d1d7703
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-21 20:26:54 -0300

    added lib victory lib to implement account stats screen

--- frontend/src/components/Account/AccountStatistics.js
+++ frontend/src/components/Account/AccountStatistics.js
@@ -1,15 +1,38 @@
 import React from "react";
 import Head from "../../Helper/Head";
+import { STATS_GET } from "../../Api";
+import Loader from "../Loader";
+//import PhotoStatsGraph from "../Photos/PhotoStatsGraphs";
+const PhotoStatsGraph = React.lazy(() => import("../Photos/PhotoStatsGraphs"));
 
 const AccountStatistics = ({ setPageTitle }) => {
+  const [loading, setLoading] = React.useState(true);
+  const [data, setData] = React.useState(null);
+
+  async function getStats() {
+    const { url, options } = STATS_GET();
+    const response = await fetch(url, options);
+    const json = await response.json();
+
+    if (response.ok) {
+      console.log("json: ", json);
+      setData(json);
+    }
+
+    setLoading(false);
+  }
+
   React.useEffect(() => {
     setPageTitle("Estatísticas");
+    getStats();
   }, []);
+
+  if (loading) return <Loader />;
   return (
-    <div>
+    <React.Suspense fallback={<div></div>}>
       <Head title="Estatísticas" />
-      conta estatisticas
-    </div>
+      <PhotoStatsGraph data={data} />
+    </React.Suspense>
   );
 };
 
