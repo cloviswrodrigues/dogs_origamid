d2e0929ed12c1821b82611cc63dbd0d7554944d8
cloviswrodrigues <cloviswrodrigues@gmail.com>
2023-04-24 23:18:42 -0300

    implemented menu mobile

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink, useLocation } from "react-router-dom";
 import style from "./Account.module.css";
 import { ReactComponent as FeedSvg } from "../../assets/feed.svg";
 import { ReactComponent as StatisticsSvg } from "../../assets/estatisticas.svg";
@@ -11,16 +11,41 @@ import AccountStatistics from "./AccountStatistics";
 import AccountPost from "./AccountPost";
 import { UserContext } from "../../Context/UserContext";
 import PageNotFound from "../PageNotFound";
+import useMedia from "../../Hooks/useMedia";
 
 const Account = () => {
   const [pageTitle, setPageTitle] = React.useState("");
+  const mobile = useMedia("(max-width: 64rem)");
+  const [showMenuMobile, setShowMenuMobile] = React.useState(true);
   const { userLogout } = React.useContext(UserContext);
+
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setShowMenuMobile(false);
+  }, [pathname]);
+
+  function toggleMenuHamburguer() {
+    setShowMenuMobile((prevState) => !prevState);
+  }
+
   return (
     <main className={style.account}>
       <section className="container">
         <div className={style.accountHeader}>
           <h1 className="title-1">{pageTitle}</h1>
-          <nav className={style.menuDesktop}>
+          {mobile && (
+            <button
+              className={`${style.buttonHamburger} ${
+                showMenuMobile && style.buttonHamburgerActive
+              }`}
+              onClick={toggleMenuHamburguer}
+            ></button>
+          )}
+          <nav
+            className={`${mobile ? style.menuMobile : style.menu} ${
+              showMenuMobile && style.menuMobileActive
+            }`}
+          >
             <NavLink
               to="/conta"
               end
@@ -29,6 +54,7 @@ const Account = () => {
               }
             >
               <FeedSvg />
+              {mobile && "Minhas Fotos"}
             </NavLink>
             <NavLink
               to="/conta/estatisticas"
@@ -37,6 +63,7 @@ const Account = () => {
               }
             >
               <StatisticsSvg />
+              {mobile && "Estatísticas"}
             </NavLink>
             <NavLink
               to="/conta/postar"
@@ -45,44 +72,13 @@ const Account = () => {
               }
             >
               <AddSvg />
+              {mobile && "Adicionar Foto"}
             </NavLink>
             <button onClick={userLogout}>
               <LogoffSvg />
+              {mobile && "Sair"}
             </button>
           </nav>
-          <div>
-            <button className={style.menuHamburger}></button>
-            <nav className={style.menuMobile}>
-              <NavLink
-                to="/conta"
-                end
-                className={({ isActive }) =>
-                  isActive ? style.active : undefined
-                }
-              >
-                <FeedSvg />
-              </NavLink>
-              <NavLink
-                to="/conta/estatisticas"
-                className={({ isActive }) =>
-                  isActive ? style.active : undefined
-                }
-              >
-                <StatisticsSvg />
-              </NavLink>
-              <NavLink
-                to="/conta/postar"
-                className={({ isActive }) =>
-                  isActive ? style.active : undefined
-                }
-              >
-                <AddSvg />
-              </NavLink>
-              <button onClick={userLogout}>
-                <LogoffSvg />
-              </button>
-            </nav>
-          </div>
         </div>
         <Routes>
           <Route
